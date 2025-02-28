076a71b99a5b4ef7aebe8b51982b312a4edb973e
deepjoshi11th <deepjoshi11th@gmail.com>
2025-02-28 12:38:25 +0530

    Decluttering files

--- js/script.js
+++ js/script.js
@@ -16,13 +16,9 @@ document.addEventListener('DOMContentLoaded', (event) => {
 function toggleDarkMode() {
     document.body.classList.toggle('dark-mode');
     const icon = document.querySelector('.dark-mode-toggle i');
-    /*const col=document.head.style.th;*/
     if (document.body.classList.contains('dark-mode')) {
         icon.classList.replace('fa-moon-o', 'fa-sun-o');
-       
-        /*col = #222222;*/
     } else {
         icon.classList.replace('fa-sun-o', 'fa-moon-o');
-        /*col = #dddddd;*/
     }
 }
